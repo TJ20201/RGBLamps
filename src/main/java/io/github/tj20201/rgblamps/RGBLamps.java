7f748461293b4a03a45196055218287ddf5bcbe9
TJ20201 <86313252+TJ20201@users.noreply.github.com>
2022-11-20 18:27:34 +0000

    Add run folder to Git

--- src/main/java/io/github/tj20201/rgblamps/RGBLamps.java
+++ src/main/java/io/github/tj20201/rgblamps/RGBLamps.java
@@ -14,7 +14,7 @@ import net.minecraft.util.registry.Registry;
 
 public class RGBLamps implements ModInitializer {
 
-        public static final Block ColorLamp = new CustomLampBlock();
+    public static final Block ColorLamp = new CustomLampBlock();
 
     @Override
     public void onInitialize() {
