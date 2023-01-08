2089b40d2a440b4114a5b0485cd34c5c19b8dabe
TJ20201 <86313252+TJ20201@users.noreply.github.com>
2023-01-08 19:47:33 +0000

    Complete 1.19.3 update for publishing

--- src/main/java/io/github/tj20201/rgblamps/RGBLamps.java
+++ src/main/java/io/github/tj20201/rgblamps/RGBLamps.java
@@ -3,9 +3,11 @@ package io.github.tj20201.rgblamps;
 import io.github.tj20201.rgblamps.block.CustomLampBlock;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 import net.minecraft.registry.Registry;
@@ -17,6 +19,10 @@ public class RGBLamps implements ModInitializer {
     @Override
     public void onInitialize() {
         Registry.register(Registries.BLOCK, new Identifier("rgblamps", "colorlamp"), ColorLamp);
-        Registry.register(Registries.ITEM, new Identifier("rgblamps", "colorlamp"), new BlockItem(ColorLamp, new FabricItemSettings().group(ItemGroup.REDSTONE)));
+        Registry.register(Registries.ITEM, new Identifier("rgblamps", "colorlamp"), new BlockItem(ColorLamp, new FabricItemSettings()));
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register(content -> {
+            content.add(ColorLamp);
+        });
     }
 }
